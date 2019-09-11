9baff0fc56dfb492bc8fa4d18468d04d570ba8d4
Stephen MacVicar <smacvicar@mitre.org>
2019-09-11 08:14:40 -0400

    update network extension url

--- lib/pdex.rb
+++ lib/pdex.rb
@@ -16,7 +16,7 @@ module PDEX
   COMMUNICATION_PROFICIENCY_EXTENSION_URL = 'http://hl7.org/fhir/uv/vhdir/StructureDefinition/communication-proficiency'
   CONTACT_POINT_AVAILABLE_TIME_EXTENSION_URL = 'http://hl7.org/fhir/uv/vhdir/StructureDefinition/contactpoint-availabletime'
   ENDPOINT_USE_CASE_EXTENSION_URL = 'http://hl7.org/fhir/uv/vhdir/StructureDefinition/endpoint-usecase'
-  NETWORK_EXTENSION_URL = 'http://hl7.org/fhir/uv/vhdir/StructureDefinition/network-reference'
+  NETWORK_EXTENSION_URL = 'http://hl7.org/fhir/us/davinci-pdex-plan-net/StructureDefinition/plannet-Network-extension'
   NEW_PATIENTS_EXTENSION_URL = 'http://hl7.org/fhir/uv/vhdir/StructureDefinition/newpatients'
   NEW_PATIENT_PROFILE_EXTENSION_URL = 'http://hl7.org/fhir/uv/vhdir/StructureDefinition/newpatientprofile'
   PRACTITIONER_QUALIFICATION_EXTENSION_URL = 'http://hl7.org/fhir/uv/vhdir/StructureDefinition/practitioner-qualification'
