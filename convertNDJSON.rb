6f0b5aeaf9b261c49fee9f77e938a9165320b653
Saul Kravitz <saul@mitre.org>
2020-12-08 18:09:34 -0500

    added export.json output

--- convertNDJSON.rb
+++ convertNDJSON.rb
@@ -1,10 +1,17 @@
 require 'json'
 require 'pry'
     indir = ARGV[0]   # point to output directory
-
+    
+    ndouts = []
     Dir.glob("#{indir}/*") do |typedir|
         puts "working on input directory: #{typedir}..."
-        outfile = "#{File.basename(typedir)}.ndjson"
+        resourceType = File.basename(typedir)
+        outfile = "#{resourceType}.ndjson"
+        ndouts << {
+            "type" => resourceType,
+            "url" => outfile
+        }
+        outfile = "#{resourceType}.ndjson"
         puts "writing to #{outfile}"
         o = File.open(outfile,"w")
         Dir.glob("#{typedir}/*.json") do |jsonfile|
@@ -15,4 +22,14 @@ require 'pry'
         end
         o.close
     end
-  
+    output = {
+        "transactionTime" => Time.now.strftime("%d/%m/%Y %H:%M"),
+        "request" => "<baseFhirURL>/$export",
+        "requiresAccessToken" => false,
+        "output" => ndouts,
+        "error" => { "type" => "OperationOutcome",
+                    "url" =>  "http://serverpath2/err_file_1.ndjson"}
+    }
+    export = File.open("export.json","w")
+    export.write(JSON.pretty_generate(output))
+ 
