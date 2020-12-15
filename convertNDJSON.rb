bd5700b877986d10187dc144feeb6f3b115d7c4c
Benjamin Langley <blangley@mitre.org>
2020-12-15 09:45:31 -0500

    Save NDJSON files to export directory

--- convertNDJSON.rb
+++ convertNDJSON.rb
@@ -1,35 +1,39 @@
 require 'json'
 require 'pry'
-    indir = ARGV[0]   # point to output directory
+
+# FHIR_SERVER_BASE = "http://localhost:8080/plan-net/"
+FHIR_SERVER_BASE = "http://davinci-plan-net-ri.logicahealth.org/plan-net/"
     
-    ndouts = []
-    Dir.glob("#{indir}/*") do |typedir|
-        puts "working on input directory: #{typedir}..."
-        resourceType = File.basename(typedir)
-        outfile = "#{resourceType}.ndjson"
-        ndouts << {
-            "type" => resourceType,
-            "url" => outfile
-        }
-        outfile = "#{resourceType}.ndjson"
-        puts "writing to #{outfile}"
-        o = File.open(outfile,"w")
-        Dir.glob("#{typedir}/*.json") do |jsonfile|
-            puts "working on: #{jsonfile}..."
-            s = File.read(jsonfile)
-            h = JSON.parse(s)
-            o.puts(JSON.generate(h))
-        end
-        o.close
-    end
-    output = {
-        "transactionTime" => Time.now.strftime("%d/%m/%Y %H:%M"),
-        "request" => "<baseFhirURL>/$export",
-        "requiresAccessToken" => false,
-        "output" => ndouts,
-        "error" => { "type" => "OperationOutcome",
-                    "url" =>  "http://serverpath2/err_file_1.ndjson"}
+ndouts = []
+FileUtils.mkdir_p("export/")
+Dir.glob("output/*") do |typedir|
+    puts "working on input directory: #{typedir}..."
+    resourceType = File.basename(typedir)
+    url = "#{resourceType}.ndjson"
+    ndouts << {
+        "type" => resourceType,
+        "url" => url
     }
-    export = File.open("export.json","w")
-    export.write(JSON.pretty_generate(output))
- 
+    outfile = "export/#{resourceType}.ndjson"
+    puts "writing to #{outfile}"
+    o = File.open(outfile,"w")
+    Dir.glob("#{typedir}/*.json") do |jsonfile|
+        puts "working on: #{jsonfile}"
+        s = File.read(jsonfile)
+        h = JSON.parse(s)
+        o.puts(JSON.generate(h))
+    end
+    o.close
+end
+output = {
+    "transactionTime" => Time.now.strftime("%d/%m/%Y %H:%M"),
+    "request" => "#{FHIR_SERVER_BASE}/$export",
+    "requiresAccessToken" => false,
+    "output" => ndouts,
+    "error" => { "type" => "OperationOutcome",
+                "url" =>  "http://serverpath2/err_file_1.ndjson"}
+}
+export = File.open("export/export.json","w")
+export.write(JSON.pretty_generate(output))
+puts("Files written to /export")
+
